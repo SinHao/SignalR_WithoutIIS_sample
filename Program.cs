2bbb81a97727d801c4baf31e79e4aed50f918bf4
SinHao <sos80065@yahoo.com.tw>
2015-11-10 17:11:40 +0800

    first

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.Hosting;
 
-namespace SignalR_withoutIIS_sample
+namespace SignalR_WithoutIIS_sample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string url = "http://localhost:5001";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.Read();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+            string exeFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string webFolder = Path.Combine(exeFolder, "Web");
+            app.UseFileServer(new FileServerOptions()
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = new PhysicalFileSystem(webFolder),
+                EnableDirectoryBrowsing = false
+            });
+        }
+    }
+
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
         }
     }
 }
